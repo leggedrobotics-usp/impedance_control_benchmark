36667bada417473dd6ecda772be6744462587b72
Leonardo <leonardo.felipe.santos@usp.br>
2024-01-14 23:09:17 -0300

    Subplots adjusted, set new colors and line styles

--- plots/subplots_figures.py
+++ plots/subplots_figures.py
@@ -1,7 +1,5 @@
 import matplotlib.pyplot as plt
-from matplotlib import rcParams
-
-rcParams["axes.labelpad"] = 0
+from matplotlib import colormaps
 
 import math
 import numpy
@@ -11,80 +9,103 @@ t_begin = 700  # samples [dt = 0.001 s]
 
 joint_impedance_js = numpy.load("data/js_data_jointimp.npy")[t_begin:, :]
 joint_impedance_ts = numpy.load("data/ts_data_jointimp.npy")[t_begin:, :]
-task_impedance_js  = numpy.load("data/js_data_taskimp.npy")[t_begin:, :]
-task_impedance_ts  = numpy.load("data/ts_data_taskimp.npy")[t_begin:, :]
+task_impedance_js = numpy.load("data/js_data_taskimp.npy")[t_begin:, :]
+task_impedance_ts = numpy.load("data/ts_data_taskimp.npy")[t_begin:, :]
 
-ts_color = "#2166ad"
-js_color = "#b2182c"
+ts_color = "#1f78b4"  # colormaps['tab20b'](1)
+js_color = "#33a02c"  # colormaps['tab20b'](13)
 font = {
     "family": "serif",
     "math_fontfamily": "cm",
     "color": "black",
-    "weight": "semibold",
+    "weight": "bold",
     "size": 13,
 }
 
 subplot_cols = 2
 subplot_rows = 3
-ieee_width_in = 3.5 + 0.25 + 3.5
+ieee_width_in = 7  # + 0.25 + 3.5
 aspect_ratio = 9 / 16
 
-# Task-space Impedance Controller
-#time = task_impedance_ts[t_offset:, 0]  # [s]
-#ee_pos = task_impedance_ts[t_offset:, 1]  # [m]
-#ee_vel = task_impedance_ts[t_offset:, 2]  # [m/s]
-#ee_int = task_impedance_ts[t_offset:, 3]  # [N]
-#ee_acc = task_impedance_ts[t_offset:, 4]  # [m/ss]
+scaling = math.e
+joint_impedance_ts[:, 1] = scaling * joint_impedance_ts[:, 1]
+joint_impedance_ts[:, 2] = scaling * joint_impedance_ts[:, 2]
 
 plt.figure(figsize=[ieee_width_in, aspect_ratio * ieee_width_in])
 
-ax1 = plt.subplot2grid((subplot_rows, subplot_cols), (1,0), colspan=1, xticklabels=[], yticklabels=[])
-
+ax1 = plt.subplot2grid(
+    (subplot_rows, subplot_cols), (1, 0), colspan=1, xticklabels=[], yticklabels=[]
+)
 ax1.plot(task_impedance_ts[:, 1], task_impedance_ts[:, 3], color=ts_color)
 ax1.plot(joint_impedance_ts[:, 1], joint_impedance_ts[:, 3], color=js_color)
 ax1.set_xlabel("$e_x$", fontdict=font)
 ax1.set_ylabel("$f_{int}$", fontdict=font)
 
-ax2 = plt.subplot2grid((subplot_rows, subplot_cols), (0,0), colspan=1, xticklabels=[], yticklabels=[])
+joint = 1
+ax2 = plt.subplot2grid(
+    (subplot_rows, subplot_cols), (0, 0), colspan=1, xticklabels=[], yticklabels=[]
+)
 ax2.plot(
-    rad2deg * task_impedance_js[:, 2],
-    task_impedance_js[:, 8],
+    rad2deg * task_impedance_js[:, joint],
+    task_impedance_js[:, joint + 6],
     color=ts_color,
-    linestyle="-.",
+    linestyle="--",
 )
 ax2.plot(
-    rad2deg * joint_impedance_js[:, 2],
-    joint_impedance_js[:, 8],
+    rad2deg * joint_impedance_js[:, joint],
+    joint_impedance_js[:, joint + 6],
     color=js_color,
-    linestyle="-.",
+    linestyle="--",
 )
-ax2.set_ylabel("$tau$", fontdict=font)
 ax2.set_xlabel("$q$", fontdict=font)
+ax2.set_ylabel(r"$\tau$", fontdict=font)
 
 
-ax3 = plt.subplot2grid((subplot_rows, subplot_cols), (0,1), rowspan=2, projection="3d", xticklabels=[], yticklabels=[], zticklabels=[])
+ax3 = plt.subplot2grid(
+    (subplot_rows, subplot_cols),
+    (0, 1),
+    rowspan=2,
+    projection="3d",
+    xticklabels=[],
+    yticklabels=[],
+    zticklabels=[],
+)
+
 ax3.set_proj_type("ortho")
-ax3.plot(task_impedance_ts[:, 1], task_impedance_ts[:, 2], task_impedance_ts[:, 3], color=ts_color)
-ax3.plot(joint_impedance_ts[:, 1], joint_impedance_ts[:, 2], joint_impedance_ts[:, 3], color=js_color)
-ax3.view_init(elev=20, azim=-20)
-
+ax3.plot(
+    task_impedance_ts[:, 1],
+    task_impedance_ts[:, 2],
+    task_impedance_ts[:, 3],
+    color=ts_color,
+)
+ax3.plot(
+    joint_impedance_ts[:, 1],
+    joint_impedance_ts[:, 2],
+    joint_impedance_ts[:, 3],
+    color=js_color,
+)
 ax3.set_xlabel("$e_x$", fontdict=font)
 ax3.set_ylabel("$\dot{e}_x$", fontdict=font)
 ax3.set_zlabel("$f_{int}$", fontdict=font)
+ax3.view_init(elev=20, azim=-20)
 
-ax4 = plt.subplot2grid((subplot_rows, subplot_cols), (2,0), colspan=1, xticklabels=[], yticklabels=[])
+ax4 = plt.subplot2grid(
+    (subplot_rows, subplot_cols), (2, 0), colspan=1, xticklabels=[], yticklabels=[]
+)
 ax4.plot(task_impedance_ts[:, 1], task_impedance_ts[:, 2], color=ts_color)
 ax4.plot(joint_impedance_ts[:, 1], joint_impedance_ts[:, 2], color=js_color)
 ax4.set_ylabel("$\dot{e}_x$", fontdict=font)
 ax4.set_xlabel("$e_x$", fontdict=font)
 
-ax5 = plt.subplot2grid((subplot_rows, subplot_cols), (2,1), colspan=1, xticklabels=[], yticklabels=[])
+ax5 = plt.subplot2grid(
+    (subplot_rows, subplot_cols), (2, 1), colspan=1, xticklabels=[], yticklabels=[]
+)
 ax5.plot(task_impedance_ts[:, 2], task_impedance_ts[:, 3], color=ts_color)
 ax5.plot(joint_impedance_ts[:, 2], joint_impedance_ts[:, 3], color=js_color)
 ax5.set_ylabel("$f_{int}$", fontdict=font)
 ax5.set_xlabel("$\dot{e}_x$", fontdict=font)
 
 plt.subplots_adjust(
-    left=0.11, bottom=0.10, right=0.9, top=0.90, wspace=0.23, hspace=0.26
+    left=0.06, bottom=0.09, right=0.94, top=1.0, wspace=0.16, hspace=0.35
 )
 plt.show(block=True)
