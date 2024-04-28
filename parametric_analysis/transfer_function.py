4bcef609cd73a9043e6b76abec11c693a74fb822
Leonardo Santos <44267124+qleonardolp@users.noreply.github.com>
2024-04-27 22:12:28 -0300

    Simplified Tz. Plots matching using Ai/wi

--- parametric_analysis/transfer_function.py
+++ parametric_analysis/transfer_function.py
@@ -5,12 +5,12 @@ import control
 from math import pi, sin, cos
 
 # Lumped parameters:
-wi = 6.2831853  # input angular frequency
-Ai = 50.00  # input amplitude
-Mr = 1.000  # actual mass
-Kd = 100.0  # impedance stiffness
-Dd = 20.00  # impedance damping
-Md = 0.100  # impedance inertia
+wi = 2*pi*0.8    # input *angular frequency
+Ai = 0.500       # input amplitude
+Mr = 1.500       # actual mass
+Kd = 1000.0      # impedance stiffness
+Dd = 2*sqrt(Kd)  # impedance damping
+Md = 0.00  # impedance inertia
 
 #print(math.sqrt(Kd/Md))
 parameters_dict = {"Kd": Kd, "Dd": Dd, "Md": Md, "wi": wi, "Mr": Mr}
@@ -21,12 +21,12 @@ x_ref_time = Ai * np.sin(wi * time)
 
 ellipse = []
 Tx, Ty, Tz = transforms(parameters_dict)
-for k in np.linspace(0, 2*pi, 256):
-  ellipse.append(Ai * Tx @ Ty @ Tz @ np.array([sin(k), cos(k)]).T)
+for k in np.linspace(0, 2*pi, 180):
+  ellipse.append(Ai/wi * Tx @ Ty @ Tz @ np.array([sin(k), cos(k)]).T) # why /wi ??
 ellipse = np.array(ellipse)
 
-s = control.tf('s')    # s-domain variable
-Z = Md*s**2 + Dd*s + Kd # s-domain impedance
+s = control.tf('s')      # s-domain variable
+Z = Md*s**2 + Dd*s + Kd  # s-domain impedance
 x_ref_laplace = Ai * wi/(s**2 + wi**2) # equilibrium point sinusoidal transfer function
 ref_to_error_tf = (Mr * s**2)/(Z + Mr * s**2)
 
@@ -39,19 +39,26 @@ ref_to_error_tf = (Mr * s**2)/(Z + Mr * s**2)
 """
 x_error_laplace = ref_to_error_tf * x_ref_laplace
 
-# control.step_response(x_error_laplace).plot()
+# control.step_response(x_error_laplace)
 error_response = control.forced_response(ref_to_error_tf, time, x_ref_time, transpose=True)
 error_derivative = np.gradient(error_response.x[0], error_response.t)
+error_2nd_derivative = np.gradient(error_derivative, error_response.t)
+force_int_time = Md * error_2nd_derivative + Dd * error_derivative + Kd * error_response.x[0]
 
 fig = plt.figure()
 ax1 = fig.add_subplot(1, 2, 1, projection="3d")
+
 ax1.set_proj_type("ortho")
-ax1.plot(error_response.x[0], error_derivative, error_response.u[0])
-ax1.plot(ellipse[:, 0], ellipse[:, 1], ellipse[:, 2],
-  color="k", linestyle="--",
-)
+ax1.plot(error_response.x[0], error_derivative, force_int_time)
+ax1.plot(ellipse[:, 0], ellipse[:, 1], ellipse[:, 2], color="r", linestyle="--")
+ax1.set_xlabel("$e$")
+ax1.set_ylabel("$\dot{e}$")
+ax1.set_zlabel("$f_{int}$")
+
 ax2 = fig.add_subplot(1, 2, 2)
-ax2.plot(error_response.t, error_response.x[0], label="position error")
-ax2.plot(error_response.t, error_derivative, label="velocity error")
-ax2.plot(error_response.t, error_response.u[0], label="$f_{int}$")
+ax2.plot(error_response.t, error_response.x[0], label="$e$")
+ax2.plot(error_response.t, error_derivative, label="$\dot{e}$")
+ax2.plot(error_response.t, force_int_time, label="$f^d_{int}$")
+ax2.plot(error_response.t, error_response.u[0], label="$x_{ref}$", color="k", linestyle="--")
+plt.legend()
 plt.show(block=True)
--- parametric_analysis/transforms_function.py
+++ parametric_analysis/transforms_function.py
@@ -1,5 +1,5 @@
 import numpy as np
-import math
+from math import sin, cos, atan, asin, sqrt
 
 def transforms(arg_dict):
     """Compute the ellipse-based transforms for a 2º Order LTI impedance controller
@@ -25,52 +25,45 @@ def transforms(arg_dict):
 
     binormal_vector = np.array([K - M * wi**2, D, -1])
 
-    rho = math.atan(D)
-    phi = math.asin(-binormal_vector[0] / np.linalg.norm(binormal_vector))
+    rho = atan(D)
+    phi = asin(-binormal_vector[0] / np.linalg.norm(binormal_vector))
 
     Tx = np.array(
         [
             [1, 0, 0],
-            [0, math.cos(rho), -math.sin(rho)],
-            [0, math.sin(rho), math.cos(rho)],
+            [0, cos(rho), -sin(rho)],
+            [0, sin(rho), cos(rho)],
         ]
     )
     Ty = np.array(
         [
-            [math.cos(phi), 0, math.sin(phi)],
+            [cos(phi), 0, sin(phi)],
             [0, 1, 0],
-            [-math.sin(phi), 0, math.cos(phi)],
+            [-sin(phi), 0, cos(phi)],
         ]
     )
     
-    sigma_10 = (D**2 + 1)
-    sigma_09 = 2 * M * Mr * wi**4
-    sigma_08 = Mr**2 * wi**4
-    sigma_07 = (D * wi)**2
-    sigma_06 = M**2 * wi**4
-    sigma_05 = K - M * wi**2
-    sigma_04 = 2 * K * M * wi**2
-    sigma_03 = math.sqrt(sigma_10) * (
-        K**2 - 2 * K * (M + Mr) * wi**2 +
-        sigma_06 + sigma_07 + sigma_08 + sigma_09
-    )
-    sigma_02 = K**2 - sigma_04 + sigma_06 + (
-        math.sqrt(sigma_10) * math.sqrt(
-            D**2 - K**2 + sigma_04 - sigma_06 + abs(sigma_05)**2 + 1)
-    )
+    sigma_08 = 2 * K * M * wi**2
+    sigma_07 = (M*wi**2)**2
+    sigma_06 = 2 * M * Mr * wi**4
+    sigma_05 = (Mr*wi**2)**2
+    sigma_04 = (D * wi)**2
+    sigma_03 = K**2 + D**2 + 1 + sigma_07 - sigma_08
+    sigma_02 = K * (K - 2 * (M + Mr) * wi**2) + sigma_04 + sigma_05 + sigma_06 + sigma_07
+    sigma_01 = D**2 + 1
     
-    sigma_01 = math.sqrt(sigma_10) * math.sqrt(sigma_05**2 + sigma_10) * (
-        K**2 - 2 * K * Mr * wi**2 - sigma_04 + sigma_06 + sigma_07 + sigma_08 + sigma_09
+    R11 = ((M + Mr) * wi**2 - K) * sqrt(sigma_03) / (
+        sqrt(sigma_01) * (
+                K**2 - 2 * K * Mr * wi**2 +
+                sigma_04 + sigma_05 + sigma_06 + sigma_07 - sigma_08
+            )
     )
-    
-    R11 = Mr * wi**2 * ((M + Mr) * wi**2 - K) * sigma_02 / sigma_01
-    R12 = D * Mr * wi**3 * sigma_02 / sigma_01
-    R21 = D * Mr * wi**2 * (
-        wi**2 * (Mr*((Mr + M)*wi**2 - K) - 1) / sigma_03
-        - 1 / math.sqrt(sigma_10)
+    R12 = D * wi * sigma_03 / (
+        sqrt(sigma_01) * sqrt((K - M * wi**2)**2 + D**2 + 1) * sigma_02
     )
-    R22 = Mr * wi**3 * ((M + Mr*sigma_10) * wi**2 - K) / sigma_03
+    R21 = D / sqrt(sigma_01) * (wi**2 * (Mr*((Mr + M)*wi**2 - K) - 1) / sigma_02 - 1)
+    R22 = wi * ((M + Mr*sigma_01) * wi**2 - K) / (sqrt(sigma_01) * sigma_02)
     
-    Tz = np.array([[R11, R12], [R21, R22], [0, 0]])
+    Tz = np.array([[R11, R12], [R21, R22], [0, 0]]) * Mr * wi**2
 
     return Tx, Ty, Tz
